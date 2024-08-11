98c60b68052dd22675b084ecc08e4938b7715f46
KavyanjaliLingala <goudkavyanjali3@gmail.com>
2024-08-11 22:07:46 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,40 @@
+#different data types in Python
+
+#integer (int)
+number_of_students = 30
+print(number_of_students)
+print(type(number_of_students))
+
+#Float(float)
+height= 6.8967
+print(height)
+print(type(height))
+
+#String (str)
+error_message= "Invalid"
+print(error_message)
+print(type(error_message))
+
+#List (list)
+favorite_foods = ["Pizza", "Burger", "Sushi"]
+print(favorite_foods)  
+print(type(favorite_foods))
+
+#Set (set)
+skills = {"Python", "Java","Java", "JavaScript", "Python"}
+print(skills)  
+print(type(skills))
+
+#Dictionary (dict)
+person_info = {"name": "John Doe", "age": 25, "city": "New York"}
+print(person_info)  
+print(type(person_info))
+
+#boolean (bool)
+is_married = False
+print(is_married)  
+print(type(is_married))  
+
+
+
+
