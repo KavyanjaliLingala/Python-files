b2d233b69cf1cfd7a2ec467017f2b523f53f386f
KavyanjaliLingala <goudkavyanjali3@gmail.com>
2024-08-11 22:07:25 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,19 @@
+ #Variable is a name given to a value.
+#Assigning string value to the variable
+studentName = "Kavyanjali"
+print(studentName)
+
+
+#Assigning Integer value to the variable
+age="21"
+print(age)
+
+#Assigning value to the variable
+x=5
+y=2
+z=x+y
+print(z)
+
+# Using Variables in a String
+intro = f"Hello, my name is {studentName},I am {age} years old and I have {z} cars."
+print(intro) 
